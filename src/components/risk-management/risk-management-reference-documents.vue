4e1fa829fbc2bbbaa993751c72ffde611a2c39e3
Sai <sai.praneeth@gmail.com>
2022-10-19 07:54:00 -0500

    Add styles to reference documents

--- src/components/risk-management/risk-management-reference-documents.vue
+++ src/components/risk-management/risk-management-reference-documents.vue
@@ -1,11 +1,40 @@
 <template>
- <div>
-  <p> Reference these documents:</p>
-  <div>{{documentStatus.documentsCompletePercent}} Complete</div>
-  <div>{{documentStatus.documentsControlScore}} Score</div>
-  <div>{{documentStatus.documentsMaturityLevel}} Maturity Level</div>
-
- </div>
+ <div class="container">
+  <div class="level">
+    <div class="level-left">
+      <div class="level-item">
+        <p class="level-left"> Reference these documents:</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="columns">
+    <div class="column">
+      <p class="is-size-4">{{documentStatus.documentsCompletePercent}}</p>
+      <div class="columns">
+        <div class="column">
+        <p> Complete</p>
+        </div>
+       </div>
+      </div>
+    <div class="column">
+      <p class="is-size-4 has-text-primary">{{documentStatus.documentsControlScore}}</p>
+      <div class="columns">
+        <div class="column">
+        <p> Score</p>
+        </div>
+       </div>
+      </div>
+    <div class="column">
+      <p class="is-size-4">{{documentStatus.documentsMaturityLevel}}</p>
+      <div class="columns">
+        <div class="column">
+        <p> Maturity Level</p>
+        </div>
+       </div>
+      </div>
+  </div> 
+  </div>
 </template>
 
 <script>
